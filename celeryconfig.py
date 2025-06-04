55c1d64eff18a7ea0a098d02d64b61f18f877dad
gab <gabcam071118.rg@gmail.com>
2025-06-03 23:43:14 -0600

    Remove expired token cleanup task from Celery tasks

--- celeryconfig.py
+++
@@ -1,8 +0,0 @@
-from celery.schedules import crontab
-
-beat_schedule = {
-    "clean-expired-tokens-every-hour": {
-        "task": "src.tasks.clean_expired_tokens_task",
-        "schedule": crontab(minute="*/1"),  # Ejecutar cada hora en punto
-        },
-    }
